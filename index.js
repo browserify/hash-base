52ed60ae6b114c5dc022bb8d16b115a7251a1916
Jimmy Wärting <jimmy@warting.se>
2022-08-01 16:55:44 +0200

    removed old dependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
-var Buffer = require('safe-buffer').Buffer
-var Transform = require('readable-stream').Transform
-var inherits = require('inherits')
+
+const { Buffer } = require('node:buffer')
+const { Transform } = require('node:stream')
 
 function throwIfNotStringOrBuffer (val, prefix) {
   if (!Buffer.isBuffer(val) && typeof val !== 'string') {
@@ -9,87 +9,95 @@ function throwIfNotStringOrBuffer (val, prefix) {
   }
 }
 
-function HashBase (blockSize) {
-  Transform.call(this)
+class HashBase extends Transform {
+  constructor (blockSize) {
+    super()
 
-  this._block = Buffer.allocUnsafe(blockSize)
-  this._blockSize = blockSize
-  this._blockOffset = 0
-  this._length = [0, 0, 0, 0]
-
-  this._finalized = false
-}
-
-inherits(HashBase, Transform)
-
-HashBase.prototype._transform = function (chunk, encoding, callback) {
-  var error = null
-  try {
-    this.update(chunk, encoding)
-  } catch (err) {
-    error = err
-  }
-
-  callback(error)
-}
-
-HashBase.prototype._flush = function (callback) {
-  var error = null
-  try {
-    this.push(this.digest())
-  } catch (err) {
-    error = err
-  }
-
-  callback(error)
-}
-
-HashBase.prototype.update = function (data, encoding) {
-  throwIfNotStringOrBuffer(data, 'Data')
-  if (this._finalized) throw new Error('Digest already called')
-  if (!Buffer.isBuffer(data)) data = Buffer.from(data, encoding)
-
-  // consume data
-  var block = this._block
-  var offset = 0
-  while (this._blockOffset + data.length - offset >= this._blockSize) {
-    for (var i = this._blockOffset; i < this._blockSize;) block[i++] = data[offset++]
-    this._update()
+    this._block = Buffer.allocUnsafe(blockSize)
+    this._blockSize = blockSize
     this._blockOffset = 0
-  }
-  while (offset < data.length) block[this._blockOffset++] = data[offset++]
+    this._length = [0, 0, 0, 0]
 
-  // update length
-  for (var j = 0, carry = data.length * 8; carry > 0; ++j) {
-    this._length[j] += carry
-    carry = (this._length[j] / 0x0100000000) | 0
-    if (carry > 0) this._length[j] -= 0x0100000000 * carry
+    this._finalized = false
   }
 
-  return this
-}
+  _transform (chunk, encoding, callback) {
+    var error = null
+    try {
+      this.update(chunk, encoding)
+    } catch (err) {
+      error = err
+    }
 
-HashBase.prototype._update = function () {
-  throw new Error('_update is not implemented')
-}
+    callback(error)
+  }
 
-HashBase.prototype.digest = function (encoding) {
-  if (this._finalized) throw new Error('Digest already called')
-  this._finalized = true
+  _flush (callback) {
+    var error = null
+    try {
+      this.push(this.digest())
+    } catch (err) {
+      error = err
+    }
 
-  var digest = this._digest()
-  if (encoding !== undefined) digest = digest.toString(encoding)
+    callback(error)
+  }
 
-  // reset state
-  this._block.fill(0)
-  this._blockOffset = 0
-  for (var i = 0; i < 4; ++i) this._length[i] = 0
+  update (data, encoding) {
+    throwIfNotStringOrBuffer(data, 'Data')
+    if (this._finalized)
+      throw new Error('Digest already called')
+    if (!Buffer.isBuffer(data))
+      data = Buffer.from(data, encoding)
 
-  return digest
-}
+    // consume data
+    var block = this._block
+    var offset = 0
+    while (this._blockOffset + data.length - offset >= this._blockSize) {
+      for (var i = this._blockOffset; i < this._blockSize;)
+        block[i++] = data[offset++]
+      this._update()
+      this._blockOffset = 0
+    }
+    while (offset < data.length)
+      block[this._blockOffset++] = data[offset++]
 
-HashBase.prototype._digest = function () {
-  throw new Error('_digest is not implemented')
+    // update length
+    for (var j = 0, carry = data.length * 8; carry > 0; ++j) {
+      this._length[j] += carry
+      carry = (this._length[j] / 0x0100000000) | 0
+      if (carry > 0)
+        this._length[j] -= 0x0100000000 * carry
+    }
+
+    return this
+  }
+
+  _update () {
+    throw new Error('_update is not implemented')
+  }
+
+  digest (encoding) {
+    if (this._finalized)
+      throw new Error('Digest already called')
+    this._finalized = true
+
+    var digest = this._digest()
+    if (encoding !== undefined)
+      digest = digest.toString(encoding)
+
+    // reset state
+    this._block.fill(0)
+    this._blockOffset = 0
+    for (var i = 0; i < 4; ++i)
+      this._length[i] = 0
+
+    return digest
+  }
+
+  _digest () {
+    throw new Error('_digest is not implemented')
+  }
 }
 
 module.exports = HashBase
